da5e393db45ecb3dd07ae4b6104a0787abff1ca0
Lucas Araujo <lucasribara@gmail.com>
2023-06-25 23:44:26 -0300

    Create simple project APIs with express ando mongodb connection

---
+++ app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //url: "mongodb://localhost:27017/lucasribara_db"
+    url: "mongodb+srv://mongotut:testing123@cluster0.gxoueaa.mongodb.net/TutorialDB?retryWrites=true&w=majority"
+}
